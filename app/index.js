73970ff73222379afd39df97c5dd346b43e44ba6
Evan <evan@strategicsale.com>
2019-03-06 15:21:44 +0800

    Add theme picker

--- app/index.js
+++ app/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
-// import Home from './screens/Home';
+import Home from './screens/Home';
 // import CurrencyList from './screens/CurrencyList';
-import Options from './screens/Options';
+// import Options from './screens/Options';
+import Themes from './screens/Themes';
 
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
+  $primaryOrange: '#D57A66',
+  $primaryGreen: '#008D9D',
+  $primaryPurple: '#9E768F',
+
   $white: '#FFF',
   $border: '#E2E2E2',
   $inputText: '#797979',
@@ -13,4 +18,4 @@ EStyleSheet.build({
   $darkText: '#343434',
 });
 
-export default () => <Options />;
+export default () => <Themes />;
