3a811ec81052dfc91c580592f9e0fb97ce333205
Evan <evan@strategicsale.com>
2019-03-06 12:03:39 +0800

    Add swap button & description. Init Header

---
+++ app/components/Header/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { View, TouchableOpacity, Image } from 'react-native';
+import styles from './styles';
+
+const Header = () => null;
+
+export default Header;
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -4,11 +4,15 @@ import { View, StatusBar } from 'react-native';
 import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
 import { InputWithButton } from '../components/TextInput';
+import { ClearButton } from '../components/Button';
+import { LastConverted } from '../components/Text';
 
 const TEMP_BASE_CURRENCY = 'USD';
 const TEMP_QUOTE_CURRENCY = 'TWD';
 const TEMP_BASE_PRICE = '100';
 const TEMP_QUOTE_PRICE = '3200';
+const TEMP_CONVERSION_RATE = 32;
+const TEMP_CONVERSION_DATE = new Date();
 
 class Home extends Component {
     handlePressBaseCurrency = () => {
@@ -21,6 +25,10 @@ class Home extends Component {
 
     handleTextChange = (text) => {
         console.log(text);
+    };
+
+    handleSwapCurrency = () => {
+        console.log('press swap')
     };
 
     render() {
@@ -40,6 +48,14 @@ class Home extends Component {
                     defaultValue={TEMP_QUOTE_PRICE}
                     editable={false}
                     value={TEMP_QUOTE_PRICE} />
+                <LastConverted 
+                    date={TEMP_CONVERSION_DATE}
+                    base={TEMP_BASE_CURRENCY}
+                    quote={TEMP_QUOTE_CURRENCY}
+                    conversionRate={TEMP_CONVERSION_RATE} />
+                <ClearButton
+                    text="Reverse Currencies"
+                    onPress={this.handleSwapCurrency} />
             </Container>
         )
     }
