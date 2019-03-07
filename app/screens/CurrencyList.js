c384ce96997308e42a66e0e0689e20fe101126d2
Evan <evan@strategicsale.com>
2019-03-07 11:21:57 +0800

    Add redux layer to currency list

--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
 import { FlatList, View, StatusBar } from 'react-native';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import { ListItem, Separator } from '../components/List';
 import currencies from '../data/currencies';
-
-const TEMP_CURRENT_CURRENCY = 'USD';
+import { changeBaseCurrency, changeQuoteCurrency } from '../actions/currencies';
 
 class CurrencyList extends Component {
     static propTypes = {
         navigation: PropTypes.object,
+        dispatch: PropTypes.func,
+        baseCurrency: PropTypes.string,
+        quoteCurrency: PropTypes.string,
     }
-    handlePress = () => {
+    handlePress = (currency) => {
+        const { type } = this.props.navigation.state.params;
+        switch (type) {
+            case 'base':
+                this.props.dispatch(changeBaseCurrency(currency));
+                break;
+            case 'quote':
+                this.props.dispatch(changeQuoteCurrency(currency));
+                break;
+        }
         this.props.navigation.goBack(null);
     };
 
     render() {
+        let comparisonCurrency = this.props.baseCurrency;
+        if (this.props.navigation.state.params.type === 'quote') {
+            comparisonCurrency = this.props.quoteCurrency;
+        }
+
         return (
             <View style={{ flex: 1 }}>
                 <StatusBar barStyle="default" translucent={false} />
@@ -24,8 +41,8 @@ class CurrencyList extends Component {
                     renderItem={({ item }) => (
                         <ListItem 
                             text={item}
-                            selected={item === TEMP_CURRENT_CURRENCY}
-                            onPress={this.handlePress} />
+                            selected={item === comparisonCurrency}
+                            onPress={() => {this.handlePress(item)}} />
                     )}
                     keyExtractor={item => item}
                     ItemSeparatorComponent={Separator} />
@@ -34,4 +51,11 @@ class CurrencyList extends Component {
     }
 }
 
-export default CurrencyList;
+const mapStateToProps = (state) => {
+    return {
+        baseCurrency: state.currencies.baseCurrency,
+        quoteCurrency: state.currencies.quoteCurrency,
+    };
+};
+
+export default connect(mapStateToProps)(CurrencyList);
